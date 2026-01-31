2ebd9951db860396e06a2f0f2cd51c02e6174777
淡いなつ <93428659+lightsummer233@users.noreply.github.com>
2026-01-31 16:02:57 +0800

    Update model identifiers in PlaySettings

--- PlayCover/Views/AppSettingsView.swift
+++ PlayCover/Views/AppSettingsView.swift
@@ -222,15 +222,19 @@ struct GraphicsView: View {
                     Spacer()
                     Picker("", selection: $settings.settings.iosDeviceModel) {
                         Text("iPad Pro (12.9-inch) (1st gen) | A9X | 4GB").tag("iPad6,7")
+                        Text("iPad Pro (12.9-inch) (2nd gen) | A10X | 4GB").tag("iPad7,1")
                         Text("iPad Pro (12.9-inch) (3rd gen) | A12X | 4GB").tag("iPad8,6")
-                        Text("iPad Pro (12.9-inch) (5th gen) | M1 | 8GB").tag("iPad13,8")
+                        Text("iPad Pro (12.9-inch) (4th gen) | A12Z | 6GB").tag("iPad8,11")
+                        Text("iPad Pro (12.9-inch) (5th gen) | M1 | 8GB").tag("iPad13,9")
                         Text("iPad Pro (12.9-inch) (6th gen) | M2 | 8GB").tag("iPad14,5")
-                        Text("iPad Pro (13-inch) (7th gen) | M4 | 8GB").tag("iPad16,6")
+                        Text("iPad Pro (13-inch) (7th gen) | M4 | 8GB").tag("iPad16,5")
+                        Text("iPad Pro (13-inch) (8th gen) | M5 | 16GB").tag("iPad17,3")
                         Divider()
                         Text("iPhone 13 Pro Max | A15 | 6GB").tag("iPhone14,3")
                         Text("iPhone 14 Pro Max | A16 | 6GB").tag("iPhone15,3")
                         Text("iPhone 15 Pro Max | A17 Pro | 8GB").tag("iPhone16,2")
                         Text("iPhone 16 Pro Max | A18 Pro | 8GB").tag("iPhone17,2")
+                        Text("iPhone 17 Pro Max | A19 Pro | 12GB").tag("iPhone18,2")
                     }
                     .frame(width: 250)
                 }
